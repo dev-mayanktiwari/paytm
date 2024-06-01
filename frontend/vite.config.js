aaea9ad01b61aa272ca3e279041d03cb2ba66ed8
Mayank <mayanktiwari1212.mt@gmail.com>
2024-06-01 22:20:50 +0530

    final dd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "https://paytm-backend-final.vercel.app/",
+        target: "https://paytm-backend-final.vercel.app",
       },
     },
   },
